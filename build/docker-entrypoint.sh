e0ec029f38c9f5fac61fb9e4938bc5440a348937
NotSafeMoon <82337996+NotSafeMoon@users.noreply.github.com>
2021-07-31 05:56:45 -0700

    done.

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -15,6 +15,7 @@ if [ "$1" = "bsc" ]; then
     chmod 700 "$BSC_DATADIR"
     echo "$0: initiating blockchain"
     bsc --datadir "$BSC_DATADIR" account new init /genesis.json
+		echo "$0: setting data directory to $BSC_DATADIR"
   fi
 
   if [ ! -f $BSC_DATADIR/config.toml ]; then
@@ -22,11 +23,10 @@ if [ "$1" = "bsc" ]; then
     chmod 700 "$BSC_DATADIR"
     echo "$0: copying config.toml to $BSC_DATADIR/"
     cp /config.toml $BSC_DATADIR/
+		echo "$0: setting data directory to $BSC_DATADIR"
   fi
-
-  echo "$0: setting data directory to $BSC_DATADIR"
-
-  set -- "$@" --datadir "$BSC_DATADIR" --config "$BSC_DATADIR/config.toml" "$OPTS_1" --syncmode "$SYNC_MODE" --gcmode "$GC_MODE" --txlookuplimit "$TX_LOOKUP_LIMIT" --http.api "$HTTP_API" --http.corsdomain "$HTTP_CORS_DOMAIN" --ws.api "$WS_API" --ws.origins "$WS_ORIGINS" --graphql.corsdomain "$GRAPH_QL_CORS_DOMAIN" --identity "$IDENTITY" --graphql.vhosts "$GRAPH_QL_V_HOSTS" "$OPTS_2"
+	
+  set -- "$@" --datadir "$BSC_DATADIR" --config "$BSC_DATADIR/config.toml" $OPTS_1 --syncmode $SYNC_MODE --gcmode $GC_MODE --txlookuplimit $TX_LOOKUP_LIMIT --http.api $HTTP_API --http.corsdomain $HTTP_CORS_DOMAIN --ws.api $WS_API --ws.origins $WS_ORIGINS --graphql.corsdomain $GRAPH_QL_CORS_DOMAIN --identity $IDENTITY --graphql.vhosts $GRAPH_QL_V_HOSTS $OPTS_2
 
 fi
 
