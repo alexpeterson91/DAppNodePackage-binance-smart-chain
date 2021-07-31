3e523b5740b8585b8812957d27a6c032e94521de
NotSafeMoon <82337996+NotSafeMoon@users.noreply.github.com>
2021-07-31 04:00:00 -0700

    fix

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -26,7 +26,21 @@ if [ "$1" = "bsc" ]; then
 
   echo "$0: setting data directory to $BSC_DATADIR"
 
-  set -- "$@" --datadir "$BSC_DATADIR" --config $BSC_DATADIR/config.toml 
+  set -- "$@" --datadir "$BSC_DATADIR"
+	--config $BSC_DATADIR/config.toml
+	$OPTS_1
+	--syncmode $SYNC_MODE
+	--gcmode $GC_MODE
+	--txlookuplimit $TX_LOOKUP_LIMIT
+	--http.api $HTTP_API
+	--http.corsdomain "$HTTP_CORS_DOMAIN"
+	--ws.api $WS_API
+	--ws.origins "$WS_ORIGINS"
+	--graphql.corsdomain "$GRAPH_QL_CORS_DOMAIN"
+	--identity "$IDENTITY"
+	--graphql.vhosts "$GRAPH_QL_V_HOSTS"
+	$OPTS_2
+
 fi
 
 echo
